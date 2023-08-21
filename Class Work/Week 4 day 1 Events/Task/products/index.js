8f2fc09074846a161e3a6cfac9d46b8a06da278f
sanjaykumarBJIT <sanjay.kumar@bjitacademy.com>
2023-08-21 15:29:07 +0600

    Week 4 day 1

---
+++ Class Work/Week 4 day 1 Events/Task/products/index.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+class Product{
+    fetchAllData(){
+        const data = fs.readFileSync("./data/manga.json");
+        return JSON.parse(data);
+    }
+
+    deleteID(id){
+        const jsonData = this.fetchAllData();
+        const target = jsonData.findIndex(key => key.id === id);
+
+        if(target!=-1){
+            const updatedObject= jsonData.filter(index => index.id != id);
+            console.log("Deleted data at: "+new Date().getHours() +" : "+ new Date().getMinutes() +" : "+ new Date().getSeconds())
+            fs.writeFileSync("./data/manga.json",JSON.stringify(updatedObject));
+            //console.log(updatedObject);
+        }
+
+        // if(target!=-1){
+        //     jsonData.splice(target, 1);
+        //     fs.writeFileSync("./data/manga.json",JSON.stringify(jsonData));
+        // }
+
+        else{
+            console.log("Invalid Id");
+        }
+    }
+    updateID(id,updateParameter){
+        const jsonData = this.fetchAllData();
+        // console.log(jsonData.find(key => key.id === id));
+        const target = jsonData.findIndex(key => key.id === id);
+        
+        // if(updateParameter.name!=null){
+        //     target.name = updateParameter.name;
+        // }
+
+        if(target!=-1){
+            jsonData[target] = {...jsonData[target],...updateParameter};
+            //console.log(jsonData);
+            console.log("Updated data at: "+new Date().getHours() +" : "+ new Date().getMinutes() +" : "+ new Date().getSeconds())
+            fs.writeFileSync("./data/manga.json",JSON.stringify(jsonData));
+        }
+        else{
+            console.log("Invalid Id");
+        }
+
+        
+
+    }
+}
+
+
+
+
+module.exports = new Product();
